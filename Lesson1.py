87e1a6f0b29539539f9f6e556b586dc0e861f7e7
Beth Miano <bethernest50@gmail.com>
2020-10-23 14:51:10 +0300

    First Commit

---
+++ Lesson1.py
@@ -0,0 +1,28 @@
+
+# A program is set of instructions that you give to a computer to produce output
+# Programming rules, known as syntax
+# Programming need values/data
+# Data types: int, float(decimals), string(x-ters), lists, tuple, dictionary,sets
+# ======INT=====
+age  =  40
+marks = 500
+points = 8000
+height = 56
+# =====Floats======
+weight = 78.5
+speed = 100.2
+
+# =====Strings====
+county = "Kisii"
+name = 'Joseph'
+
+# show your variables
+print(age)
+print(marks)
+print(points)
+print(weight)
+print(county)
+print(name)
+
+
+
