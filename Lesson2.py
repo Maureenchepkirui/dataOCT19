68427e965ddfa9b6ad9ff07aa4f85457d91753dc
Beth Miano <bethernest50@gmail.com>
2020-10-27 15:19:39 +0300

    Third Commit

--- Lesson2.py
+++ Lesson2.py
@@ -6,5 +6,14 @@ print(counties)
 budgets = [500000, 200000,600000, 800000,700000,1000000]
 print(budgets)
 
+# slicing lists
 print('I come from ', counties[4], 'County')
+print('I will visit ', counties[1:4])  # -1
+print('My budget is ', budgets[3])
+
+# What is a tuple  - is a collection of items stored in one variable
+cars = ('Toyota','Nissan','BMW','Honda','Volvo')
+print(cars)
+
+
 
