1dd1e4ebe5d2864330187ba91ba08fbd3f0c32b8
Beth Miano <bethernest50@gmail.com>
2020-10-30 15:44:09 +0300

    Fifth Commit

--- Lesson4.py
+++ Lesson4.py
@@ -30,18 +30,18 @@ simple_interest()
 
 # making a function more dynamic, we use paramters
 # payroll function
-def payroll():
-    basic_salary = 50000
-    transport = 3000
-    water = 1500
+def payroll(basic_salary,transport, water):
+    # basic_salary = 50000
+    # transport = 3000
+    # water = 1500
     nhif = 500
-
     gross_pay = basic_salary + transport + water
     print('Your Gross Salary is ', gross_pay)
 
-
-payroll()
-payroll()
+# this function is more reusable
+payroll(basic_salary=30000,transport=4500,water=1500)
+payroll(basic_salary=28000, transport=3500,water=2500)
+payroll(water=2300, transport=4500,basic_salary=75000)
 
 
 
