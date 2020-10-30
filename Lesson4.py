a4f5d05dc731fc393fb74fb94a169b04eae6d954
Beth Miano <bethernest50@gmail.com>
2020-10-30 14:58:09 +0300

    Fifth Commit

---
+++ Lesson4.py
@@ -0,0 +1,27 @@
+
+# functional way
+# functional is a block of codes/statements that perform specif task
+# Advantages: split your into small bits, easy to use, maintain, understand code
+#             Code re use - use existing code to build new code
+#             Used in Object Oriented Programming
+
+def body_mass_index():
+    weight = 89.8
+    height = 1.4
+    bmi = weight/(height*height)
+    print('Your BMI ', bmi)
+
+# Function must be invoked/used/call/trigger
+body_mass_index()
+
+
+
+
+
+
+
+
+
+
+
+
