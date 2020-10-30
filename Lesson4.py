10950db8d4d4da709ef783f3f3ed04ab9765e47b
Beth Miano <bethernest50@gmail.com>
2020-10-30 15:29:55 +0300

    Fifth Commit

--- Lesson4.py
+++ Lesson4.py
@@ -24,8 +24,24 @@ def simple_interest():
     print('Your SI is ', interest)
 
 simple_interest()
+simple_interest()
+simple_interest()
 
 
+# making a function more dynamic, we use paramters
+# payroll function
+def payroll():
+    basic_salary = 50000
+    transport = 3000
+    water = 1500
+    nhif = 500
+
+    gross_pay = basic_salary + transport + water
+    print('Your Gross Salary is ', gross_pay)
+
+
+payroll()
+payroll()
 
 
 
