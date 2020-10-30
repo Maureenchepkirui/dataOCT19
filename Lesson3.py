1b4cfe00ae9a27f893b7cfc74e81380ef6321682
Beth Miano <bethernest50@gmail.com>
2020-10-30 14:31:52 +0300

    Fifth Commit

--- Lesson3.py
+++ Lesson3.py
@@ -3,9 +3,17 @@
 try:
     person = {'john':40, 'peter':45}
     print(person['hassan'])
+    f = open("modcom.txt", "r")
+
+except KeyError as key_error:
+    print('Error Occured1!', key_error)
+
+except FileNotFoundError as file_error:
+    print('Error Occured2!', file_error)
+
 except:
-    print('Error Occured!')
-    # Exception try, except
+    print('Something is wrong')
+
 finally:
     print('Close connections')
     # it will execute regardless if an error is raised or not
