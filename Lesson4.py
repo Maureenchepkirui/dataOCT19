8ffd6948422c87f52510667f6d965dbe83b647c1
Beth Miano <bethernest50@gmail.com>
2020-10-30 15:02:37 +0300

    Fifth Commit

--- Lesson4.py
+++ Lesson4.py
@@ -4,7 +4,7 @@
 # Advantages: split your into small bits, easy to use, maintain, understand code
 #             Code re use - use existing code to build new code
 #             Used in Object Oriented Programming
-
+# 1
 def body_mass_index():
     weight = 89.8
     height = 1.4
@@ -14,7 +14,16 @@ def body_mass_index():
 # Function must be invoked/used/call/trigger
 body_mass_index()
 
+# 2
+def simple_interest():
+    principle = 60000
+    rate = 12.5
+    time = 36
 
+    interest = (principle*rate*time)/100
+    print('Your SI is ', interest)
+
+simple_interest()
 
 
 
